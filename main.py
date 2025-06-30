2669c88c1c8ef1bfdcfe024c4e2060071f4378c8
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-06-30 15:02:22 +0545

    Add TodoRequest model and implement create_todo endpoint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel, Field
 import models
 from models import Todo
 from database import engine, SessionLocal
@@ -21,6 +22,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3, max_length=100)
+    description: str = Field(min_length=3, max_length=255)
+    priority: int = Field(ge=0, le=10)
+    completed: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def get_all_todos(db: db_dependency):
     return db.query(Todo).all()
@@ -33,3 +41,12 @@ async def get_todo(db: db_dependency, id: int):
         return queryset
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.post("/", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    queryset = Todo(**todo_request.model_dump())
+
+    db.add(queryset)
+    db.commit()
+    return status.HTTP_201_CREATED
