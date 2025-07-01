2b5b1fae3c88f6a934564f9848c13cd39280aaa9
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-01 23:45:56 +0545

    Switch database from SQLite to PostgreSQL and remove local SQLite database file

--- database.py
+++ database.py
@@ -2,10 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:bismillah@localhost:5432/TodoDatabase"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
