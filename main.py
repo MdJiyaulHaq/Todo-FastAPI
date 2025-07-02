bc17be93c3a4fbcae0cdd5df1d934592cc88895f
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-02 12:26:15 +0545

    Add homepage template and static CSS for styling

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import models
 from database import engine
 from routers import admin, auth, todo, users
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
