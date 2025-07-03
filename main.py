183b4b461c9ac4c97572710cb43c3b1b72c51a00
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-03 16:24:30 +0545

    Redirect root URL to todo page for improved navigation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 import models
 from database import engine
@@ -8,12 +7,12 @@ from routers import admin, auth, todo, users
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
 
 
 models.Base.metadata.create_all(bind=engine)
