33586cf6ebf0a00e0c58a849724b97d3f01aa492
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-07-03 21:48:11 +0545

    Update database connection string for production environment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:bismillah@localhost:5432/TodoDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://md:ZA71cmldH00aQ5hiDwnpzNz7yDDks6Sq@dpg-d1jaig6r433s738383j0-a/tododatabase_5lxw"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
